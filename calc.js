be6ed3565e8ab18791c16740519adbda17ce3f66
sasuke-287 <22591052+sasuke-287@users.noreply.github.com>
2023-10-09 15:39:28 +0900

    位置テスト

--- calc.js
+++ calc.js
@@ -1,112 +1,66 @@
 import LatLon from "https://cdn.jsdelivr.net/npm/geodesy@2.2.1/latlon-spherical.min.js";
 
-export class CalcVR {
-  constructor() {
-    this.distance = 0;
-    this.bearing = 0;
-    this.newPosition = [0, 0];
-    this.currentPosition = [0, 0];
-    this.objectSize = "0, 0, 0";
-    this.newDistance = 800;
-  }
-
-  calcDist(currentPosiArg, targetPosition) {
-    const current = new LatLon(currentPosiArg[0], currentPosiArg[1]);
-    const target = new LatLon(targetPosition[0], targetPosition[1]);
-    this.distance = current.distanceTo(target);
-    this.bearing = current.finalBearingTo(target);
-    this.currentPosition = currentPosiArg;
-  }
-  calcNewPosition(currentPosition, bearing, newTargetToDistance) {
-    const current = new LatLon(currentPosition[0], currentPosition[1]);
-    const calculatedlced = current.destinationPoint(
-      newTargetToDistance,
-      bearing
-    );
-    this.newPosition = [calculatedlced.latitude, calculatedlced.longitude];
-  }
-  calcSizeDist(distance) {
-    if (distance <= 1000 && distance >= 500) {
-      this.objectSize = "25 25 25";
-      this.newDistance = 800;
-    } else if (distance > 1000 && distance <= 8000) {
-      this.objectSize = "20 20 20";
-      this.newDistance = 800 + distance / 1000;
-    } else if (distance > 8000 && distance <= 16000) {
-      this.objectSize = "18 18 18";
-      this.newDistance = 800 + distance / 1000;
-    } else if (distance > 16000 && distance <= 20000) {
-      this.objectSize = "15 15 15";
-      this.newDistance = 800 + distance / 1000;
-    } else if (distance > 20000) {
-      this.objectSize = "10 10 10";
-      this.newDistance = 800 + distance / 1000;
-    }
-  }
-}
 window.onload = () => {
-  navigator.geolocation.getCurrentPosition(success, error, options);
+  if (!navigator.geolocation) return;
+  setInterval(getPosition, 1000);
 };
 
-function staticLoadPlaces() {
-  return [
-    {
-      name: "hachiban",
-      location: {
-        lat: 35.67142999196126,
-        lng: 139.77022276452675,
-      },
-    },
-  ];
+function getPosition() {
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
 }
 
-function renderPlaces(places, pos) {
-  let scene = document.querySelector("a-scene");
-  var crd = pos.coords;
-  let cal = new CalcVR();
+function onSuccess(position) {
+  const propaties = [];
+  for (var key in position.coords) {
+    propaties.push(`${key} = ${position.coords[key]}`);
+  }
+  const propatiesString = propaties.reduce((pre, cur) => pre + `\n` + cur);
 
-  places.forEach((place) => {
-    let latitude = place.location.lat;
-    let longitude = place.location.lng;
-    let name = place.name;
-    let modelName = place.modelName;
-    cal.calcDist([crd.latitude, crd.longitude], [latitude, longitude]);
-    console.log(`heading: ${crd.heading}`);
-    cal.calcNewPosition(cal.currentPosition, cal.bearing, cal.newDistance);
-    cal.calcSizeDist(cal.distance);
-    let model = document.createElement("a-ring");
-    model.setAttribute("color", "green");
-    model.setAttribute("look-at", "[gps-camera]");
-    model.setAttribute("radius-inner", "1");
-    model.setAttribute("radius-outer", "5");
-    model.setAttribute("rotation", "-90 0 0");
-    model.setAttribute(
-      "gps-entity-place",
-      `latitude: ${cal.newPosition[0]}; longitude: ${cal.newPosition[1]};`
-    );
-    model.setAttribute("scale", `2 2 2`);
+  const { distance, direction } = getDistanceAndDirection(position.coords);
 
-    model.addEventListener("loaded", () => {
-      window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
-    });
+  const viewString =
+    propatiesString +
+    `\n距離1：${distance}\n方角x:${direction.x}\n方角y:${direction.y}`;
 
-    scene.appendChild(model);
-  });
+  document.getElementById("output").innerText = viewString;
 }
-var options = {
-  enableHighAccuracy: true,
-  timeout: 50000,
-  maximumAge: 0,
+function onError(error) {
+  const propaties = [];
+  for (var key in error) {
+    propaties.push(`${key} = ${error[key]}`);
+  }
+  const propatiesString = propaties.reduce((pre, cur) => pre + `\n` + cur);
+  document.getElementById("output").innerText = propatiesString;
+}
+
+// 飲み屋座標
+const target = {
+  latitude: 35.67142999196126,
+  longitude: 139.77022276452675,
+  altitude: 45,
 };
 
-function success(pos) {
-  let places = staticLoadPlaces();
-  renderPlaces(places, pos);
+function getDistanceAndDirection(params) {
+  const selfPosition = new LatLon(params.latitude, params.longitude);
+  const targetPosition = new LatLon(target.latitude, target.longitude);
+
+  // 2座標間距離
+  const distance = selfPosition.distanceTo(targetPosition);
+
+  // 2座標間平面方向角度
+  const direction = { x: 0, y: 0 };
+  direction.x = convert(selfPosition.finalBearingTo(targetPosition));
+
+  // 2座標間垂直方向角度
+  const altitudeDiff = target.altitude - params.altitude;
+  direction.y = (Math.atan2(distance, -altitudeDiff) * 180) / Math.PI - 90;
+
+  return { distance, direction };
 }
 
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-  alert("Unable to capture current location.");
+// 北を0とした0~360度系を南を0とした0~360度系に変換
+function convert(arg) {
+  return (360 - arg + 180) % 360;
 }
 
 window.addEventListener("deviceorientationabsolute", orientationHandler, true);
